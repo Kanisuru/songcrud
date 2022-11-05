9163437ba78489728754fafae8c9345b72bf36f6
Kanisuru Daniel <kanisurudaniel@gmail.com>
2022-11-05 05:50:04 +0100

    Update admin.py

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Artiste, Song, Lyric
 
 # Register your models here.
+
+django.admin.register(Artiste)
+django.admin.register(Song)
+django.admin.register(Lyric)
